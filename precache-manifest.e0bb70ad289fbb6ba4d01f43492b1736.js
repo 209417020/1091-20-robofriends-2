e4cbb324afb0c152cca26815df11627ca8bf42cd
209417020 <lucifer890523@gmail.com>
2020-10-14 20:49:07 +0800

    Updates

---
+++ precache-manifest.e0bb70ad289fbb6ba4d01f43492b1736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8063858b004e193031f1abc9dc88630",
+    "url": "/1091-20-robofriends-2/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-20-robofriends-2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "082c1af8bbc742e907e0",
+    "url": "/1091-20-robofriends-2/static/css/main.4cc52a60.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-20-robofriends-2/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-20-robofriends-2/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082c1af8bbc742e907e0",
+    "url": "/1091-20-robofriends-2/static/js/main.c29de14b.chunk.js"
+  },
+  {
+    "revision": "e96b69e36b707e6aec91",
+    "url": "/1091-20-robofriends-2/static/js/runtime-main.d8bae197.js"
+  }
+]);
