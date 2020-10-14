f85bbfb47165fcc1f63f6d5f99e9c2e3b0234216
209417020 <lucifer890523@gmail.com>
2020-10-14 20:50:47 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/1091-20-robofriends-2/precache-manifest.e0bb70ad289fbb6ba4d01f43492b1736.js"
+  "/1091-20-robofriends-2/precache-manifest.c1a5aad8e917714f1d15a7f81e4a8b49.js"
 );
 
 self.addEventListener('message', (event) => {
